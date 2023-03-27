ac2f68028847f68431461d27bf64575e11231c55
JoshDutta <44683417+JoshDutta@users.noreply.github.com>
2023-03-26 23:31:59 -0400

    Add files via upload

--- startup.py
+++ startup.py
@@ -1,89 +1,65 @@
-import PySimpleGUI as sg
 import os
 import subprocess
 import tkinter as tk
 import requests
-# function to establish a new connection
-def createNewConnection(name, SSID, password):
-    config = """<?xml version=\"1.0\"?>
-<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
-    <name>"""+name+"""</name>
-    <SSIDConfig>
-        <SSID>
-            <name>"""+SSID+"""</name>
-        </SSID>
-    </SSIDConfig>
-    <connectionType>ESS</connectionType>
-    <connectionMode>auto</connectionMode>
-    <MSM>
-        <security>
-            <authEncryption>
-                <authentication>WPA2PSK</authentication>
-                <encryption>AES</encryption>
-                <useOneX>false</useOneX>
-            </authEncryption>
-            <sharedKey>
-                <keyType>passPhrase</keyType>
-                <protected>false</protected>
-                <keyMaterial>"""+password+"""</keyMaterial>
-            </sharedKey>
-        </security>
-    </MSM>
-</WLANProfile>"""
-    command = "netsh wlan add profile filename=\""+name+".xml\""+" interface=Wi-Fi"
-    with open(name+".xml", 'w') as file:
-        file.write(config)
-    os.system(command)
+from time import sleep
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.common.by import By
+#rom selenium.webdriver.support.ui import WebDriverWait
+#rom selenium.webdriver.support import expected_conditions as EC
 
+def start_ride():
+    
+    exec(open("/home/jdlinux/Downloads/CycOwl-main/Detector/video.py").read())
 
-# function to connect to a network   
-def connect(name, SSID):
-    command = "netsh wlan connect name=\""+name+"\" ssid=\""+SSID+"\" interface=Wi-Fi"
-    os.system(command)
-
-# function to display avavilabe Wifi networks   
-def displayAvailableNetworks():
-    command = "netsh wlan show networks interface=Wi-Fi"
-    os.system(command)
-
-def connecting():
-# input wifi name and password
-    name = clicked.get()
+def connector():
+    Wname = clicked.get()
     password = inputtxt.get(1.0, "end-1c")
-    createNewConnection(name, name, password)
-    connect(name, name)
-    timeout = 1 
+    createNewConnection(Wname,Wname,password)
+    Connect(Wname,Wname)
+    timeout = 1
     try:
-        requests.head("http://www.google.com/", timeout=timeout)
-        l = tk.Label(root, text = "Current WiFi Connection: " + clicked.get())
+        requests.head("http://google.com/", timeout=timout)
+        l = tk.label(root,text = "Current WiFi Connection: " + name)
         l.pack()
-    except requests.ConnectionError:
-        t = tk.Label(root, text = "Failed to Connect")
+    except:
+        t = tk.Label(root,text = "Failed to Connect, Try Again")
         t.pack()
 
 def holder():
     print("hello")
 
+print ("Checking for root access...")
+user = os.popen("whoami").read()
+if 'root' not in user:
+    print('You need to be the root user to run this program and you are running as '+user+'  Try sudo python <ScriptName>.py')
+    print ('Exiting...')
+    quit()
+else:
+    print('Good job, you\'re root.')
 
-# using the check_output() for having the network term retrieval
-results = subprocess.check_output(["netsh", "wlan", "show", "network"])
-results = results.decode("ascii") # needed in python 3
-results = results.replace("\r","")
-ls = results.split("\n")
-ls = ls[4:]
-ssids = []
-x = 0
-while x < len(ls):
-    if x % 5 == 0:
-        ssids.append(ls[x])
-    x += 1
-ssids = [ssid.split(':',1)[-1].strip() for ssid in ssids]
-ssidsall = []
-x = 0
-while x < len(ssids):
-    if ssids[x] != "":
-        ssidsall.append(ssids[x])
-    x+=1
+print ("Trying to activate the default wlan0 network...")
+status = os.popen("ifconfig wlan0 up").read()
+if 'No such device' in status:
+    print ("It seems your wireless device is not named wlan0, so you're going to need to enter it manually.")
+    winame = raw_input('Wireless Device Name: ')
+else:
+    winame = "wlan0"
+print ("Wireless device enabled!")
+print ("Checking for available wireless networks...")
+stream = os.popen("iwlist " + winame + " scan")
+print ("Available Networks:")
+networksfound = 0
+for line in stream:
+    if "ESSID" in line:
+        networksfound += 1
+        ssidsall.append(line.split('ESSID:"', 1)[1].split('"', 1)[0])
+if networksfound == 0:
+    print ("Looks like we didn't find any networks in your area. Exiting...")
+    ssidsall = ["Hello", "world"]
+    #quit()
 
 # Create object
 root = tk.Tk()
@@ -100,10 +76,10 @@ root.title("WIFI Password")
 inputtxt = tk.Text(root,height = 2,width = 10)
 inputtxt.pack()
 
-button = tk.Button( root , text = "Connect" , command = connecting).pack()
+button = tk.Button( root , text = "Connect To Wifi" , command = connector).pack()
 
-zoom = tk.Button( root , text = "Start Zoom" , command = holder).pack()
-ride = tk.Button( root , text = "Start Ride" , command = holder).pack()
+zoom = tk.Button( root , text = "Start Zoom" , command = holder()).pack() #exec(open("join_zoom.py").read())
+ride = tk.Button( root , text = "Start Ride" , command = ).pack()
 
 # Execute tkinter
 root.mainloop()
