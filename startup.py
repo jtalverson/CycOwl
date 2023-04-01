67240068c47c183f351104821a0c3dd98b8cbdbc
JoshDutta <44683417+JoshDutta@users.noreply.github.com>
2023-04-01 16:56:03 -0400

    Add files via upload

--- startup.py
+++ startup.py
@@ -1,100 +1,80 @@
 import os
-import subprocess
 import tkinter as tk
-import requests
-from time import sleep
-from threading import Thread
-import continuous_threading
-from scapy.all import *
-from threading import Thread
-import pandas
+import pexpect
+import sys
 import time
-import os
 
-# initialize the networks dataframe that will contain all access points nearby
-networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
-# set the index BSSID (MAC address of the AP)
-networks.set_index("BSSID", inplace=True)
+def scan():
+	termOut = pexpect.run('hcitool scan')
+	output = (termOut.decode('utf-8')).split("\r")
+	output.pop(0)
+	finalOut = []
+	for x in output:
+		x.replace('\n','')
+		finalOut.append(x.split("\t"))
+	devices = []
+	connections = []
+	for x in finalOut:
+		for y in x:
+			if y !="\n" and y != "n/a":
+				if len(y) == 17 and y[2] == ':' and y[5] == ':' and y[8] == ':' and y[11] == ':' and y[14] == ':':
+					connections.append(y)
+				else:
+					devices.append(y)
+		if(len(connections)>len(devices)):
+			connections.pop(len(connections)-1)
+		if(len(devices)>len(connections)):
+			devices.pop(len(devices)-1)
+	print(devices)
+	return devices,connections
+ 
+def connect(index,devices,connections,prev):
+	if(got != ''):
+		name = devices[index]
+		address = connections[index]
+		response = ''
+		p = pexpect.spawn('bluetoothctl',encoding='utf-8')
+		p.logfile_read = sys.stdout
+		p.expect('#')
+		if(prev != ""):
+			p.sendline("disconnect " + prev)
+			p.expect("#")
+		p.sendline("remove " +address)
+		p.sendline("scan on")
+		p.expect("#")
+		mylist = ["Discovery started","Failed to start discovery","Device "+address+" not available","Failed to connect","Connection successful"]
+		while response != "Connection successful":
+			p.expect(mylist)
+			response=p.after
+			p.sendline("connect "+address)
+			time.sleep(1)
+		prev = address
+		p.sendline("quit")
+		p.close()
+		#time.sleep(1)
+	return prev
 
-def callback(packet):
-    if packet.haslayer(Dot11Beacon):
-        # extract the MAC address of the network
-        bssid = packet[Dot11].addr2
-        # get the name of it
-        ssid = packet[Dot11Elt].info.decode()
-        try:
-            dbm_signal = packet.dBm_AntSignal
-        except:
-            dbm_signal = "N/A"
-        # extract network stats
-        stats = packet[Dot11Beacon].network_stats()
-        # get the channel of the AP
-        channel = stats.get("channel")
-        # get the crypto
-        crypto = stats.get("crypto")
-        networks.loc[bssid] = (ssid, dbm_signal, channel, crypto)
+def disconnect(prev):
+	response = ''
+	p = pexpect.spawn('bluetoothctl',encoding='utf-8')
+	p.logfile_read = sys.stdout
+	p.expect('#')
+	if(prev != ""):
+		p.sendline("disconnect " + prev)
+		p.expect("#")
+		prev = ""
+	else:
+		print("No Bluetooth Devices are connected")
+	p.sendline("quit")
+	p.close()
+	return prev
 
-
-def print_all():
-    while True:
-        os.system("clear")
-        print(networks)
-        time.sleep(0.5)
-
-
-def change_channel():
-    ch = 1
-    while True:
-        os.system(f"iwconfig {interface} channel {ch}")
-        # switch channel from 1 to 14 each 0.5s
-        ch = ch % 14 + 1
-        time.sleep(0.5)
-
-
-if __name__ == "__main__":
-    # interface name, check using iwconfig
-    interface = "wlan0mon"
-    # start the thread that prints all the networks
-    printer = Thread(target=print_all)
-    printer.daemon = True
-    printer.start()
-    # start the channel changer
-    channel_changer = Thread(target=change_channel)
-    channel_changer.daemon = True
-    channel_changer.start()
-    # start sniffing
-    sniff(prn=callback, iface=interface)
-
-""" def start_zoom():
-    exec(open("/home/jdlinux/Downloads/CycOwl-main/join_zoom.py").read())
-
-def start_ride():
-    exec(open("/home/jdlinux/Downloads/CycOwl-main/Detector/video.py").read())
-
-def holder():
-    print("hello")
- """
-""" # Create object
-root = tk.Tk()
-# Adjust size
-root.geometry( "800x480" )
-
-# datatype of menu text
-clicked = tk.StringVar()
-relist = tk.Button( root , text = "Refresh List" , command = ssid(ssidsall)).pack()
-drop = tk.OptionMenu( root , clicked , *ssidsall )
-drop.pack()
-
-#password input
-root.title("WIFI Password")
-inputtxt = tk.Text(root,height = 2,width = 10)
-inputtxt.pack()
-
-button = tk.Button( root , text = "Connect To Wifi" , command = connector).pack()
-zoom = tk.Button( root , text = "Start Zoom" , command = holder).pack() #exec(open("join_zoom.py").read())
-ride = tk.Button( root , text = "Start Ride" , command = holder).pack()
-
-# Execute tkinter
-root.mainloop()
-
- """
+prev = ""
+devices = []
+connections = []
+devices,connections = scan()
+print("here")
+got = input()
+got = int(got)
+connect(got,devices,connections,prev)
+disconnect(prev)
