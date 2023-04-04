33a200f911d9f51e7fe04f440e7af3074f32b4f7
JoshDutta <44683417+JoshDutta@users.noreply.github.com>
2023-04-04 13:16:02 -0400

    Add files via upload

--- testFull.py
+++ testFull.py
@@ -12,9 +12,8 @@ class App():
         self.selectedB = ""
         scan(self)
         print(self.bdevices)
-        print("here")
         self.tmp = 0
-        self.tmp = int(input)
+        self.tmp = int(input("HERE: "))
         connect(self)
         self.wdevices = []
         self.wpass = ""
@@ -67,28 +66,28 @@ def disconnect(self):
     p.close()
 		
 def scan(self):
-		termOut = pexpect.run('hcitool scan')
-		output = (termOut.decode('utf-8')).split("\r")
-		output.pop(0)
-		finalOut = []
-		for x in output:
-			x.replace('\n','')
-			finalOut.append(x.split("\t"))
-		devices = []
-		connections = []
-		for x in finalOut:
-			for y in x:
-				if y !="\n" and y != "n/a":
-					if len(y) == 17 and y[2] == ':' and y[5] == ':' and y[8] == ':' and y[11] == ':' and y[14] == ':':
-						connections.append(y)
-					else:
-						devices.append(y)
-			if(len(connections)>len(devices)):
-				connections.pop(len(connections)-1)
-			if(len(devices)>len(connections)):
-				devices.pop(len(devices)-1)
-		self.bdevices = devices
-		self.bmacs = connections
+    termOut = pexpect.run('hcitool scan')
+    output = (termOut.decode('utf-8')).split("\r")
+    output.pop(0)
+    finalOut = []
+    for x in output:
+        x.replace('\n','')
+        finalOut.append(x.split("\t"))
+    devices = []
+    connections = []
+    for x in finalOut:
+        for y in x:
+            if y !="\n" and y != "n/a":
+                if len(y) == 17 and y[2] == ':' and y[5] == ':' and y[8] == ':' and y[11] == ':' and y[14] == ':':
+                    connections.append(y)
+                else:
+                    devices.append(y)
+        if(len(connections)>len(devices)):
+            connections.pop(len(connections)-1)
+        if(len(devices)>len(connections)):
+            devices.pop(len(devices)-1)
+    self.bdevices = devices
+    self.bmacs = connections
 
 def scanWifi(self):
     command = '/bin/bash -c "sudo iw wlan0 scan | grep -Po \'(signal|SSID):\K.*\' | sed \'s/ $/ [unknown SSID]/\' | paste -d  - - | cut -c2- | sort -gr"'
