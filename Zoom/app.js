e43a8aa11427ef19ca48a0cb7842622b04e68368
drewfox3 <122465756+drewfox3@users.noreply.github.com>
2023-04-14 18:07:40 -0400

    Update again
    Should be fixed

--- Zoom/app.js
+++ Zoom/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const crypto = require('crypto');
+const path = require('path');
+
+const API_KEY = 'QSad0m5NQkq2XoeT8s8osw';
+const API_SECRET = 'nouE624RlF7aXznFSIKvN6Yjb0x6244z';
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
-app.use(express.static('zoom-sdk'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.post('/api/signature', (req, res) => {
+  const meetingNumber = req.body.meetingNumber;
+  const role = req.body.role || 0;
+  const timestamp = new Date().getTime() - 30000;
+  const msg = Buffer.from(API_KEY + meetingNumber + timestamp + role).toString('base64');
+  const hash = crypto.createHmac('sha256', API_SECRET).update(msg).digest('base64');
+  const signature = Buffer.from(`${API_KEY}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64');
+  res.json({ signature });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(3000, () => console.log('Server listening on port 3000!'));
