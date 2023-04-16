65ca984a55b8fbabe2c728fabb2c64b6280fdf8e
jtalverson <jtal228@uky.edu>
2023-04-16 12:34:21 -0400

    major changes

--- detection/speakWarning.py
+++ detection/speakWarning.py
@@ -11,5 +11,5 @@ long_path = "/home/" + getpass.getuser() + "/CycOwl/detection/"
 mp3_path = long_path + "allMP3s/"
 
 print("Subprocess received label of: " + args.label)
-os.system("mpg321 \"" + mp3_path + args.label + ".mp3\"")
+os.system("mpg321 -o alsa \"" + mp3_path + args.label + ".mp3\"")
 os.system("echo false > " + long_path + "currentlySpeaking.txt")
--- detection/speech_distance_detection.py
+++ detection/speech_distance_detection.py
@@ -14,12 +14,11 @@ import pexpect
 
 objectWidths = {}
 
-canSpeak = True
 warningStack = list()
 timeBetweenWarnings = 15
 lastWarnTime = {}
 
-warnDistance = 30 * 12
+warnDistance = {}
 
 allSubprocesses = {}
 
@@ -32,11 +31,12 @@ print("Generating warnings...")
 os.system("python " + long_path + "generateWarnings.py")
 print("Done!")
 
-with open(long_path + "objectWidths.txt") as allWidths:
-        widths = allWidths.readlines()
-        for width in widths:
-                currentSplit = width.split(',')
-                objectWidths[currentSplit[0]] = float(currentSplit[1].strip())
+with open(long_path + "objectWidths_Distances.txt") as allWidthsDistances:
+	data = allWidthsDistances.readlines()
+	for entry in data:
+		currentSplit = entry.split(',')
+		objectWidths[currentSplit[0]] = float(currentSplit[1])
+		warnDistance[currentSplit[0]] = float(currentSplit[2].strip())
 
 print (objectWidths)
 
@@ -68,69 +68,84 @@ net = jetson_inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson_utils.gstCamera(1280, 720, "0")
 display = jetson_utils.videoOutput("display://0")
 
-while display.IsStreaming():
-	with open(long_path + "canSpeak.txt") as speakControl:
-		for line in speakControl.readlines():
+os.system("pacmd set-default-sink alsa_output.usb-Solid_State_System_Co._Ltd._USB_PnP_Audio_Device_000000000000-00.analog-stereo")
+
+control = True
+full_allow_path = long_path + "process.txt"
+while control:
+	if not display.IsStreaming():
+		control = False
+
+	startProcessing = False
+	with open(full_allow_path) as allow:
+		allowance = allow.readlines()
+		for line in allowance:
 			if line.strip() == "true":
-				canSpeak = True
-			else:
-				canSpeak = False
+				startProcessing = True
 
-	img, width, height = camera.CaptureRGBA()
-	detections = net.Detect(img, width, height)
-	display.Render(img)
-	display.SetStatus("Object Detection | Network: {:0f} FPS".format(net.GetNetworkFPS()))
+	if not startProcessing:
+		img, width, height = camera.CaptureRGBA()	
+		display.Render(img)
+		display.SetStatus("Detection not started")
+		print ("Not yet processing")
+		time.sleep(.5)
+	else:
+		img, width, height = camera.CaptureRGBA()
+		display.Render(img)
+		detections = net.Detect(img, width, height)	
+		display.SetStatus("Object Detection | Network: {:0f} FPS".format(net.GetNetworkFPS()))
 
-	# If there are no objects detected in the current frame
-	if (len(detections) == 0):
-		# Update the is talking variable
-		isTalking = False
-		with open(long_path + "currentlySpeaking.txt") as speakControl:
-			for line in speakControl.readlines():
-				if line.strip() == "true":
-					isTalking = True
-		print(isTalking)
-		# Pull any warnings off of the warning stack
-		if len(warningStack) > 0 and not isTalking:
-			allSubprocesses[warningStack[0]] = subprocess.Popen(["python3.6", long_path + "speakWarning.py", warningStack[0]])
-			os.system("echo true > " + long_path + "currentlySpeaking.txt")
-			warningStack.remove(warningStack[0])
+		# If there are no objects detected in the current frame
+		if (len(detections) == 0):
+			# Update the is talking variable
+			isTalking = False
+			with open(long_path + "currentlySpeaking.txt") as speakControl:
+				for line in speakControl.readlines():
+					if line.strip() == "true":
+						isTalking = True
+			print(isTalking)
+			# Pull any warnings off of the warning stack
+			if len(warningStack) > 0 and not isTalking:
+				allSubprocesses[warningStack[0]] = subprocess.Popen(["python3.6", long_path + "speakWarning.py", warningStack[0]])
+				os.system("echo true > " + long_path + "currentlySpeaking.txt")
+				warningStack.remove(warningStack[0])
 
-	for detection in detections:
-		currentLabel = net.GetClassDesc(detection.ClassID)
-		inches = -1
-		closeEnough = False
-		enoughTime = False
-		isTalking = False
-		if currentLabel in objectWidths:
-			inches = distance_to_camera(BASE_WIDTH, focalLength, (BASE_WIDTH / objectWidths[currentLabel]) * detection.Width)
-			if inches < warnDistance:
-				closeEnough = True
+		for detection in detections:
+			currentLabel = net.GetClassDesc(detection.ClassID)
+			inches = -1
+			closeEnough = False
+			enoughTime = False
+			isTalking = False
+			if currentLabel in objectWidths:
+				inches = distance_to_camera(BASE_WIDTH, focalLength, (BASE_WIDTH / objectWidths[currentLabel]) * detection.Width)
+				if inches < warnDistance[currentLabel] * 12:
+					closeEnough = True
 
-			if abs(time.time() - lastWarnTime[currentLabel]) > timeBetweenWarnings:
-				enoughTime = True
-				lastWarnTime[currentLabel] = time.time()
+				if abs(time.time() - lastWarnTime[currentLabel]) > timeBetweenWarnings:
+					enoughTime = True
+					lastWarnTime[currentLabel] = time.time()
 
-		with open(long_path + "currentlySpeaking.txt") as speakControl:
-			for line in speakControl.readlines():
-				if line.strip() == "true":
-					isTalking = True
-		print(isTalking)
+			with open(long_path + "currentlySpeaking.txt") as speakControl:
+				for line in speakControl.readlines():
+					if line.strip() == "true":
+						isTalking = True
+			print(isTalking)
 
-		print ("Close %s Time %s Talking %s" % (closeEnough, enoughTime, isTalking))
+			print ("Close %s Time %s Talking %s" % (closeEnough, enoughTime, isTalking))
 
-		name = os.path.join(mp3_path, currentLabel + ".mp3")
-		# print(name)
-		if len(warningStack) > 0 and not isTalking:
-			allSubprocesses[warningStack[0]] = subprocess.Popen(["python3.6", long_path + "speakWarning.py", warningStack[0]])
-			os.system("echo true > " + long_path + "currentlySpeaking.txt")
-			warningStack.remove(warningStack[0])
-			isTalking = True
-		elif os.path.isfile(name) and canSpeak and closeEnough and enoughTime and not isTalking:
-			allSubprocesses[currentLabel] = subprocess.Popen(["python3.6", long_path + "speakWarning.py", currentLabel])
-			os.system("echo true > " + long_path + "currentlySpeaking.txt")
-		
-		if os.path.isfile(name) and canSpeak and closeEnough and enoughTime and isTalking and currentLabel not in warningStack:
-			warningStack.append(currentLabel)
+			name = os.path.join(mp3_path, currentLabel + ".mp3")
+			# print(name)
+			if len(warningStack) > 0 and not isTalking:
+				allSubprocesses[warningStack[0]] = subprocess.Popen(["python3.6", long_path + "speakWarning.py", warningStack[0]])
+				os.system("echo true > " + long_path + "currentlySpeaking.txt")
+				warningStack.remove(warningStack[0])
+				isTalking = True
+			elif os.path.isfile(name) and closeEnough and enoughTime and not isTalking:
+				allSubprocesses[currentLabel] = subprocess.Popen(["python3.6", long_path + "speakWarning.py", currentLabel])
+				os.system("echo true > " + long_path + "currentlySpeaking.txt")
+			
+			if os.path.isfile(name)  and closeEnough and enoughTime and isTalking and currentLabel not in warningStack:
+				warningStack.append(currentLabel)
 
 os.system("echo false > " + long_path + "currentlySpeaking.txt")
+os.system("echo false > " + full_allow_path)
